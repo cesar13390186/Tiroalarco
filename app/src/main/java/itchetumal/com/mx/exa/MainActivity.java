4b2bf720fa8fec8511bedff317b7a4c8492c7ca6
cesar13390186 <villanueva.sansores@gmail.com>
2016-11-28 18:47:57 -0500

    PRUEBA 2

--- app/src/main/java/itchetumal/com/mx/exa/MainActivity.java
+++ app/src/main/java/itchetumal/com/mx/exa/MainActivity.java
@@ -14,15 +14,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  {
 
-    float cirCoordX = 0, cirCoordY = 0, coordInicX = 0, coordInicY=0;
+    float cirCoordX = 0, cirCoordY = 0;
     Path ruta = new Path();
-    String mensaje = " ", mensaje2 = " ";
-    int Tiro = 0;
-    int puntos,jugador1,jugador2;
-
-    public void jugadores() {
-        Toast.makeText(this, "Jugado uno", Toast.LENGTH_LONG).show();
-    }
+    String mensaje = " ";
+    int jugador1 = 0, jugador2=0;
+    int Tiro=0,op=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +31,40 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+    public void jugadores(int tiro, int puntos) {
+        int div = tiro/2;
+        if( div == op) {
+            jugador1 = jugador1 + puntos;
+            op++;
+        }else {
+            jugador2 = jugador2 + puntos;
+        }
+
+        if (tiro == 20){
+            if (jugador1 > jugador2){
+                Toast.makeText(this, "ganador jugador 1", Toast.LENGTH_SHORT).show();
+            }else if (jugador1 < jugador2){
+                Toast.makeText(this, "ganador jugador 2", Toast.LENGTH_SHORT).show();
+            }else if(jugador1 == jugador2){
+                Toast.makeText(this, "draw game", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private int ConTiros() {
+        Tiro ++;
+        return Tiro;
+    }
+
     class Lienzo extends View{
 
-        boolean clickOrigen = false;
         int ancho;
         int alto;
 
         public Lienzo(Context context) {
             super(context);
         }
-        public void onDraw(Canvas canvas){
+        public void onDraw(Canvas canvas) {
 
             Paint pincel = new Paint();
             ancho = canvas.getWidth();
@@ -52,9 +72,9 @@ public class MainActivity extends AppCompatActivity  {
             pincel.setAntiAlias(true);
 
             pincel.setTextSize(50);
-            canvas.drawText(mensaje,0,this.getMeasuredHeight(),pincel);
+            canvas.drawText(mensaje, 0, this.getMeasuredHeight(), pincel);
 
-            float x1=ancho/2, y1=alto/2;
+            float x1 = ancho / 2, y1 = alto / 2;
 
             pincel.setColor(Color.GREEN);
             canvas.drawCircle(x1, y1, 150, pincel);
@@ -65,9 +85,11 @@ public class MainActivity extends AppCompatActivity  {
             pincel.setColor(Color.RED);
             canvas.drawCircle(x1, y1, 50, pincel);
 
-            canvas.drawText("Tiros: "+mensaje2,50,50,pincel);
-            canvas.drawText("Puntos: "+puntos,50,100,pincel);
+            canvas.drawText("Tiros: " + Tiro, 50, 50, pincel);
+             //canvas.drawText("Puntos: "+puntos,50,100,pincel);
 
+            canvas.drawText("Puntos 1: "+jugador1,50,150,pincel);
+            canvas.drawText("Puntos 2: "+jugador2,50,200,pincel);
 
         }
 
@@ -76,41 +98,31 @@ public class MainActivity extends AppCompatActivity  {
 
             cirCoordX = evento.getX();
             cirCoordY = evento.getY();
+            int puntos=0;
 
-            if (Tiro == 0){
-                jugadores();
+            if(Tiro<=20) {
+
+                if (evento.getAction() == MotionEvent.ACTION_DOWN) {
+                    ruta.moveTo(cirCoordX, cirCoordY);
+                    ConTiros();
+
+                    double cordenadas = Math.sqrt(Math.pow(cirCoordX - (ancho / 2), 2) + Math.pow(cirCoordY - (alto / 2), 2));
+
+                    if ( cordenadas <= 150) {
+                        puntos = puntos + 1;
+                    }
+                    if (cordenadas <= 100) {
+                        puntos = puntos + 1;
+                    }
+                    if (cordenadas <= 50) {
+                        puntos = puntos + 1;
+                    }
+                    jugadores(Tiro, puntos);
+                }
+                this.invalidate();
+                return true;
             }
-
-            if (evento.getAction()== MotionEvent.ACTION_DOWN){
-                ruta.moveTo(cirCoordX,cirCoordY);
-                mensaje = "Evento Down ";
-                mensaje2 = ConTiros();
-
-                if (Math.sqrt(Math.pow(cirCoordX-(ancho/2), 2) + Math.pow(cirCoordY-(alto/2),2)) <= 150){
-                    mensaje = "Diste click en el círculo VERDE";
-                    puntos = puntos +1;
-                    clickOrigen = true;
-                }
-
-                if (Math.sqrt(Math.pow(cirCoordX-(ancho/2), 2) + Math.pow(cirCoordY-(alto/2),2)) <= 100){
-                    mensaje = "Diste click en el círculo azul";
-                    puntos = puntos +1;
-                    clickOrigen = true;
-                }
-                if (Math.sqrt(Math.pow(cirCoordX-(ancho/2), 2) + Math.pow(cirCoordY-(alto/2),2)) <= 50){
-                    mensaje = "Diste click en el círculo rojo";
-                    puntos = puntos +1;
-                    clickOrigen = true;
-                }
-
-            }
-            this.invalidate();
-            return true;
+            return false;
         }
-    }
-
-    private String ConTiros() {
-        Tiro ++;
-        return String.valueOf(Tiro);
     }
 }
